ff9be3b6cca5d6470cea62a2eb922c1ec4793e86
Kenny Meyer <knny.myer@gmail.com>
2015-03-18 12:16:04 -0300

    Remove rollbar

--- Gemfile
+++ Gemfile
@@ -263,4 +263,3 @@ end
 
 # Riffle-specific gems
 gem 'rails_12factor', group: :production
-gem 'rollbar'
--- config/initializers/rollbar.rb
+++
@@ -1,4 +0,0 @@
-Rollbar.configure do |config|
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
-  config.use_sidekiq
-end
