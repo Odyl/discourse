654ce64e0219b01a6d6e3ae4465d4d3e2f136e7c
Kenny Meyer <knny.myer@gmail.com>
2015-03-19 13:30:44 -0300

    Revert "Use Puma as web server"
    This reverts commit 49726d2d8529b830b2b7fde9ec0e5beedfcb9e97.

--- config/puma.rb
+++ config/puma.rb
@@ -1,31 +1,15 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+# First, you need to change these below to your situation.
+APP_ROOT = '/home/discourse/discourse'
+num_workers = ENV["NUM_WEBS"].to_i > 0 ? ENV["NUM_WEBS"].to_i : 4
 
+# Second, you can choose how many threads that you are going to run at same time.
+workers "#{num_workers}"
+threads 8,32
+
+# Unless you know what you are changing, do not change them.
+bind  "unix://#{APP_ROOT}/tmp/sockets/puma.sock"
+stdout_redirect "#{APP_ROOT}/log/puma.log","#{APP_ROOT}/log/puma.err.log"
+pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
+state_path "#{APP_ROOT}/tmp/pids/puma.state"
+daemonize true
 preload_app!
-
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
-
-#First, you need to change these below to your situation.
-#APP_ROOT = '/home/discourse/discourse'
-#num_workers = ENV["NUM_WEBS"].to_i > 0 ? ENV["NUM_WEBS"].to_i : 4
-
- #Second, you can choose how many threads that you are going to run at same time.
-#workers "#{num_workers}"
-#threads 8,32
-
- #Unless you know what you are changing, do not change them.
-#bind  "unix://#{APP_ROOT}/tmp/sockets/puma.sock"
-#stdout_redirect "#{APP_ROOT}/log/puma.log","#{APP_ROOT}/log/puma.err.log"
-#pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
-#state_path "#{APP_ROOT}/tmp/pids/puma.state"
-#daemonize true
-#preload_app!
